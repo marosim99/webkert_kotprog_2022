21a7a47f7813084d295d92db91b3e8325458cdc6
Marosi Márk Dániel <marosim99@gmail.com>
2022-05-08 13:37:19 +0200

    re-added API key

--- webkert-ticket/src/environments/environment.ts
+++ webkert-ticket/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     appId: '1:711278563684:web:2910466bc5a47ec495ea71',
     storageBucket: 'mobilalk-ticket.appspot.com',
     locationId: 'europe-west',
-    apiKey: '',
+    apiKey: 'AIzaSyCcdwZE5btT-szcj5YC5RSR1HXxqtZ7KKI',
     authDomain: 'mobilalk-ticket.firebaseapp.com',
     messagingSenderId: '711278563684',
   },
